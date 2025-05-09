d7f08f9c9c51e3f82fecbae08e753de3ad35265c
timka <svintsovtima@gmail.com>
2025-05-09 23:33:34 +0300

    final

---
+++ js/task-1.js
@@ -0,0 +1,30 @@
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  get Balance() {
+    return this.balance;
+  },
+  get Discount() {
+    return this.discount;
+  },
+  set Discount(value) {
+    this.discount = value;
+  },
+  get Orders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
